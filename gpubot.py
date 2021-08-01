0a74e54516c58f4bbe89464f6f6d0fc29973b07f
Jared Cleghorn <jaredrcleghorn@gmail.com>
2021-08-01 17:58:51 -0400

    Add stock checking

--- gpubot.py
+++ gpubot.py
@@ -2,9 +2,14 @@ from chromedriver_py import binary_path
 import coloredlogs
 import json
 import logging
+from lxml import html
+from random_user_agent.params import HardwareType, Popularity, SoftwareType
+from random_user_agent.user_agent import UserAgent
+import requests
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
+import time
 
 # create logger
 logger = logging.getLogger(__name__)
@@ -50,3 +55,42 @@ except:
 finally:
     # destroy browser
     browser.quit()
+
+# create user agent rotator
+user_agent_rotator = UserAgent(hardware_types=[HardwareType.COMPUTER.value], software_types=[SoftwareType.WEB_BROWSER.value], popularity=[Popularity.POPULAR.value])
+
+while True:
+    # calc next time
+    next_time = time.time() + config['delay']
+
+    # randomize user agent
+    user_agent = user_agent_rotator.get_random_user_agent()
+    logger.debug(f'using user agent {user_agent}')
+
+    logger.info('checking stock')
+
+    # send ajax request
+    start_time = time.time()
+    r = requests.get(f"https://smile.amazon.com/gp/aod/ajax?asin={config['asin']}", cookies={'session-id': ''}, headers={'user-agent': user_agent})
+    logger.debug(f'ajax request took {int(1000 * (time.time() - start_time))} ms')
+    logger.debug(f'ajax request returned status code {r.status_code}')
+
+    if r.status_code == 200:
+        offer_divs = html.fromstring(r.text).xpath("//div[@id='aod-sticky-pinned-offer'] | //div[@id='aod-offer']")
+
+        for offer_div in offer_divs:
+            price_spans = offer_div.xpath(".//span[@class='a-price-whole']")
+
+            if price_spans:
+                price = int(price_spans[0].text.replace(',', ''))
+                logger.info(f'offer for ${price}')
+
+                # check price
+                if config['min_price'] <= price <= config['max_price']:
+                    offering_id = offer_div.xpath(".//input[@name='offeringID.1']")[0].value
+                    logger.debug(f'offering_id = {offering_id}')
+
+    # sleep time left
+    time_left = next_time - time.time()
+    if time_left > 0:
+        time.sleep(time_left)
